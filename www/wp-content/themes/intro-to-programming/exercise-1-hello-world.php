616d7db636d2a41adc23a88602eaf7cae9f93eb6
peterhslutsky <peterhslutsky@gmail.c.om>
2014-09-16 16:20:55 -0600

    Exercise 2
    Implemented Exercise Two

--- www/wp-content/themes/intro-to-programming/exercise-1-hello-world.php
+++ www/wp-content/themes/intro-to-programming/exercise-1-hello-world.php
@@ -1,1 +1,3 @@
-// Code goes here
+<strong>
+<?php echo 'Hello World!';?>
+</strong>
--- www/wp-content/themes/intro-to-programming/exercise-2-random.php
+++ www/wp-content/themes/intro-to-programming/exercise-2-random.php
@@ -1,1 +1,14 @@
-// Code here
+<?php
+$rand = rand (0 , 100) ;
+
+function print_random ($number){
+if ( $number < 50 ) {
+echo "<font color='blue'>$number</font>";
+} else {
+  echo "<font color='red'>$number</font>";
+}
+ }
+
+print_random ($rand);
+
+ ?>
